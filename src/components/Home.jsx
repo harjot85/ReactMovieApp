797b29427f0feb150fb57f8257cdc43099202beb
Harjot <singh.harjot85@gmail.com>
2020-05-28 16:50:18 -0700

    Add hook to fetch the data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import {
+  API_URL,
+  API_KEY,
+  IMAGE_BASE_URL,
+  BACKDROP_SIZE,
+  POSTER_SIZE,
+} from "../config";
 
-import HeroImage from './elements/HeroImage'
-import Spinner from './elements/Spinner'
-import Grid from './elements/Grid'
-import MovieThumbs from './elements/MovieThumbs'
-import LoadMoreButton from './elements/LoadMoreButton'
-import SearchBar from './elements/SearchBar'
+// Components
+import HeroImage from "./elements/HeroImage";
+import Spinner from "./elements/Spinner";
+import Grid from "./elements/Grid";
+import MovieThumbs from "./elements/MovieThumbs";
+import LoadMoreButton from "./elements/LoadMoreButton";
+import SearchBar from "./elements/SearchBar";
 
-const Home = () => (
+// Hooks
+import { useHomeFetch } from "./hooks/useHomeFetch";
+
+const Home = () => {
+  const [{ state, loading, error }] = useHomeFetch();
+  console.log("State: ", state)
+  return (
     <>
-    <HeroImage />
-    <SearchBar />
-    <Grid />
-    <MovieThumbs />
-    <LoadMoreButton />
-    <Spinner />
+      <HeroImage />
+      <SearchBar />
+      <Grid />
+      <MovieThumbs />
+      <LoadMoreButton />
+      <Spinner />
     </>
-)
+  );
+};
 
-export default Home
+export default Home;
