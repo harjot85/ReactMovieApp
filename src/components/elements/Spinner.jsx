5698e2a1a0e58ce3d1ce3cd7a290d48055b14bd6
Harjot <singh.harjot85@gmail.com>
2020-06-14 17:23:34 -0700

    Add Spinner

--- src/components/elements/Spinner.jsx
+++ src/components/elements/Spinner.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
+import {StyledSpinner} from '../styles/StyledSpinner'
 
-const Spinner = () => <div>Spinner</div>
+const Spinner = () => <div><StyledSpinner /></div>
 
 export default Spinner
